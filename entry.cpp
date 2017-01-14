d3b91522ed435b719a785c5414fe748c19fe7f22
CrystalGrape <2424994234@qq.com>
2017-01-14 23:55:29 +0800

    使用命令行参数启动程序

--- entry.cpp
+++ entry.cpp
@@ -2,16 +2,20 @@
 #include "VirtualMachine.h"
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 	try
 	{
+		if (argc != 2)
+		{
+			std::cout << "demo:assembly-n [asn file]" << std::endl;
+			return 0;
+		}
 		std::vector<std::string> OriginalCode;
 		VirtualMachine vm(4096);
-		/*vm.ImportExe("demo.n");*/
-		vm.LoadModule(OriginalCode, "demo.main");
+		vm.LoadModule(OriginalCode, argv[1]);
 		vm.Compile(OriginalCode);
-		
+
 		vm.Run();
 	}
 	catch (VMExpection e)
